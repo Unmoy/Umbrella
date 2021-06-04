fd05dead7f960404bb7ca843115640dece33d82e
Unmoy <vampireklug@gmail.com>
2021-06-04 20:39:54 +0600

    footer created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Footer from "../Footer/Footer";
 import HomeChair from "../HomeChair/HomeChair";
 import HomeMain from "../HomeMain/HomeMain";
+import Inspiration from "../Inspiration/Inspiration";
+import Newsletter from "./Newsletter/Newsletter";
 
 const Home = () => {
   return (
     <div>
       <HomeMain></HomeMain>
       <HomeChair></HomeChair>
+      <Inspiration></Inspiration>
+      <Newsletter></Newsletter>
+      <Footer></Footer>
     </div>
   );
 };
